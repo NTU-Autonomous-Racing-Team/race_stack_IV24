7f0ea84394ffdc0bfe2c9f0cb190caa00aefbdd4
Luthov <lukettl15@gmail.com>
2024-02-06 15:35:06 +0800

    fixed controller mapping

---
+++ f1tenth_ws/build/joy_teleop/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/deepspeed/ICRA2024/f1tenth_ws/install/teleop_tools_msgs/share/teleop_tools_msgs/package.sh"
---
+++ f1tenth_ws/build/vesc_driver/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/deepspeed/ICRA2024/f1tenth_ws/install/vesc_msgs/share/vesc_msgs/package.sh"
