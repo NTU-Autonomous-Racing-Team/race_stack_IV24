3a355d914d771661c7cbaf0be413f82227dab2f8
bentjh01 <benjamintehjh01@gmail.com>
2024-02-24 18:10:59 +0800

    gap_finder update

--- f1tenth_ws/src/gap_finder/scripts/gap_finder_base.py
+++ f1tenth_ws/src/gap_finder/scripts/gap_finder_base.py
@@ -19,10 +19,12 @@ class GapFinderAlgorithm:
         self.safety_bubble_diameter = safety_bubble_diameter  # [m]
         self.scan_angle_increment = scan_angle_increment  # [rad]
         self.view_angle = 1.4  # [rad]
+        self.range_max = 30
+        self.max_range = 30
 
-        self.speed_pid = PID(Kp=-1., Ki=0.0, Kd=0.05)
+        self.speed_pid = PID(Kp=-1., Ki=0.0, Kd=0.005)
         self.speed_pid.set_point = 0.0
-        self.steering_pid = PID(Kp=-1, Ki=0.0, Kd=0.05)
+        self.steering_pid = PID(Kp=-1, Ki=0.0, Kd=0.01) 
         self.steering_pid.set_point = 0.0
 
     def limit_field_of_view(self):
@@ -39,8 +41,10 @@ class GapFinderAlgorithm:
     def generate_safety_bubble(self):
         # set the ranges of the safety bubble to 0
         self.front_range = self.ranges[int(len(self.ranges) // 2)]
+        # make safety bubble increase with viewable distance
+        safety = self.safety_bubble_diameter + self.front_range/self.range_max*1.4
         arc_increment = float(self.min_range * self.scan_angle_increment)
-        radius_count = int(self.safety_bubble_diameter / 2 / arc_increment)
+        radius_count = int(safety / 2 / arc_increment)
         for i in range(
             max(0, self.min_range_index - radius_count), min(self.min_range_index + radius_count + 1, len(self.ranges))
         ):
@@ -64,10 +68,9 @@ class GapFinderAlgorithm:
         # linear velocity is proportional to the max range
         init_linX = self.front_range
         linX = self.speed_pid.update(init_linX, self.dt)
-        print(linX)
         self.twist = [linX, angZ]
 
-    def update(self, ranges = [], dt = 0.02):
+    def update(self, ranges, dt):
         self.ranges = ranges
         self.dt = dt
         self.limit_field_of_view()
@@ -109,6 +112,7 @@ class GapFinderNode(Node):
         self.angle_min = scan_msg.angle_min
         self.angle_max = scan_msg.angle_max
         self.gapFinderAlgorithm.scan_angle_increment = scan_msg.angle_increment
+        self.gapFinderAlgorithm.range_max = scan_msg.range_max
         self.ranges = scan_msg.ranges
 
     def odom_callback(self, odom_msg):
@@ -142,7 +146,8 @@ class GapFinderNode(Node):
 
     def run(self):
         if self.ready:
-            self.twist = self.gapFinderAlgorithm.update(self.ranges, self.current_time - self.last_time)
+            dt = self.current_time - self.last_time
+            self.twist = self.gapFinderAlgorithm.update(self.ranges, dt)
             self.apply_filter()
             self.publish_drive_msg()
 
