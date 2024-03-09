32e464383e215b2593b3bb74e727b623c6b49bed
Luthov <lukettl15@gmail.com>
2024-03-09 20:04:46 +0800

    managed to get binary image

--- raceline/centerline.py
+++ raceline/centerline.py
@@ -1,31 +1,12 @@
-import cv2
 import numpy as np
+import cv2
 
-# Load the occupancy grid image
-image = cv2.imread('Austin/Austin_map.png', 0)  # Load as grayscale
+image = cv2.imread('Austin/Austin_map_grey.png', cv2.IMREAD_GRAYSCALE)
 
-# Apply edge detection
-edges = cv2.Canny(image, 30, 200)
-
-# Find contours
-contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# Sort contours by area in descending order
-contours = sorted(contours, key=cv2.contourArea, reverse=True)
-
-# Create a new image filled with zeros, the same size as the original image
-filled_image = np.zeros_like(image)
-
-# Always fill the largest contour with white color
-cv2.fillPoly(filled_image, contours[0], 255)
-
-# If there is a second contour, fill it with black color
-if len(contours) > 1:
-    cv2.fillPoly(filled_image, contours[1], 0)
-
-print(edges.shape)
+# Threshold the image to separate white areas (outside track) from the rest
+_, thresh = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
 
 # Display the result
-cv2.imshow('Final Image', filled_image)
+cv2.imshow('Result', thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
