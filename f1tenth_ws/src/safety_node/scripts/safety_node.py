74117116491baec1bb5cd6b2f962bbd745f287c5
bentjh01 <benjamintehjh01@gmail.com>
2024-03-28 02:57:26 +0800

    safety node update for aeb_ackermann

--- f1tenth_ws/src/safety_node/scripts/safety_node.py
+++ f1tenth_ws/src/safety_node/scripts/safety_node.py
@@ -3,35 +3,39 @@
 import rclpy
 from rclpy.node import Node
 from ackermann_msgs.msg import AckermannDriveStamped
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Int32
 from sensor_msgs.msg import Joy
 
 class SafetyNode(Node):
     def __init__(self):
         super().__init__('safety_node')
         # Drive Pass Through 
-        self.drive_subscription = self.create_subscription(AckermannDriveStamped, 'nav/drive', self.drive_callback, 10)
+        self.drive_subscription = self.create_subscription(AckermannDriveStamped, 'nav/drive', self.drive_callback, 1)
         self.drive_subscription 
 
-        self.teleop_subscription = self.create_subscription(AckermannDriveStamped, 'teleop', self.drive_callback, 10)
+        self.teleop_subscription = self.create_subscription(AckermannDriveStamped, 'teleop', self.drive_callback, 1)
         self.teleop_subscription 
 
-        self.drive_publisher = self.create_publisher(AckermannDriveStamped, 'drive', 10)
+        self.drive_publisher = self.create_publisher(AckermannDriveStamped, 'drive', 1)
         
         # Managed Sources
-        self.ttc_subcription = self.create_subscription(Float32, 'ttc', self.ttc_callback, 10)
+        self.ttc_subcription = self.create_subscription(Float32, 'ttc', self.ttc_callback, 1)
         self.ttc_subcription 
         self.ttc_gain = 1.0
 
-        self.joy_subscription = self.create_subscription(Joy, 'joy', self.joy_callback, 10)
+        self.joy_subscription = self.create_subscription(Joy, 'joy', self.joy_callback, 1)
         self.joy_subscription
         self.joy_gain = 1.0
+
+        self.aeb_subcription = self.create_subscription(Int32, 'aeb', self.aeb_callback, 1)
+        self.aeb_subcription 
+        self.aeb_gain = 1.0
 
         self.drive_msg = AckermannDriveStamped()
         
     def drive_callback(self, msg):
         self.drive_msg = msg
-        self.drive_msg.drive.speed *= self.joy_gain * self.ttc_gain
+        self.drive_msg.drive.speed *= self.joy_gain * self.aeb_gain # * self.ttc_gain
         self.drive_publisher.publish(self.drive_msg)
 
     def ttc_callback(self, msg):
@@ -48,6 +52,12 @@ class SafetyNode(Node):
         else:
             self.joy_gain = 0.0
             self.ttc_gain = 1.0
+            self.aeb_gain = 1.0
+
+    def aeb_callback(self, msg):
+        if msg.data:
+            self.get_logger().info('Collision Iminent')
+            self.aeb_gain = 0.0
 
 def main(args = None):
     rclpy.init(args=args)
