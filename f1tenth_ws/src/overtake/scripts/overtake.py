83e7aba8927d071f461320eb79cefc5b4d2dc641
Luthov <lukettl15@gmail.com>
2024-04-03 22:11:25 +0800

    implemented high pass filter to get rid of false positives
    Although it might not technically be an actual high pass filter doh. I
    just check for how much time has passed and not the frequency of car
    detection.

--- f1tenth_ws/src/overtake/scripts/overtake.py
+++ f1tenth_ws/src/overtake/scripts/overtake.py
@@ -19,30 +19,42 @@ class Overtake(Node):
 
         self.distance_threshold = 0.7
         self.distance_threshold_car = 0.035
+        self.car_detected_period = 0.3
+        self.car_detected = False
+        self.init_period = 0.0
 
     def scan_callback(self, scan_data):
+
         ranges = scan_data.ranges
         angle_increment = scan_data.angle_increment
+        secs = scan_data.header.stamp.sec
+        nsecs = scan_data.header.stamp.nanosec
 
         found_start = False
         found_end = False
+
         delta = float("inf")
         line_length = 0.0
 
-        # THOUGHTS: When an end is detected it could also be the start of the car?
-        # Loop through the ranges and find when there is a big jump in the distance
+        # To keep track of how long another car is detected
+        if self.car_detected:
+            if self.init_period == 0.0:
+                self.init_period = secs + nsecs * 1e-9
+            curr_period = secs + nsecs * 1e-9
+            self.car_detected_period = curr_period - self.init_period
+
+        else:
+            self.car_detected_period = 0.0
+            self.init_period = 0.0
+
         for i in range(1, len(ranges)):
 
             delta = abs(ranges[i] - ranges[i - 1])
 
-            # print(f"{i} | {ranges[i-1]:.2f} | {ranges[i]:.2f} |  {delta:.2f}")
-            # print(f"{i} | {delta:.2f}")
+            # Finding start and end of car by detecting edges using a distance_threshold
             if delta > self.distance_threshold and found_start != True:
                 found_start = True
                 start_of_car = i
-                # print(
-                #     f"Start: {i} | {ranges[i-1]:.2f} | {ranges[i]:.2f} |  {delta:.2f}"
-                # )
 
             elif (
                 delta > self.distance_threshold
@@ -51,39 +63,39 @@ class Overtake(Node):
             ):
                 found_end = True
                 end_of_car = i - 1
-                # print(f"End: {i} | {ranges[i-1]:.2f} | {ranges[i]:.2f} |  {delta:.2f}")
 
+            # Once edges found calculate the width of the range
             if found_start and found_end:
-                # print(f"Start: {start_of_car} | End: {end_of_car}")
                 for i in range(start_of_car, end_of_car):
+
                     try:
                         a = ranges[i + 1]
                         b = ranges[i]
-                        # print(f"a-b: {abs(a-b):.2f}")
 
                         if abs(a - b) < self.distance_threshold_car:
+                            if (
+                                0.25 < line_length < 0.35
+                                and i == (end_of_car - 1)
+                                and a < 20.0
+                                and b < 20.0
+                            ):
 
-                            # print(
-                            #     f"a: {a:.2f} | b: {b:.2f} | a-b: {abs(a-b):.2f} | line_length: {line_length:.2f}"
-                            # )
-
-                            if 0.25 < line_length < 0.35 and i == (end_of_car - 1):
-                                if a < 20.0 and b < 20.0:
-
+                                # Checking for false positives
+                                if self.car_detected_period > 0.5:
                                     print(f"Car detected")
-                                    # print(
-                                    #     f"a: {a:.2f} | b: {b:.2f} | a-b: {abs(a-b):.2f} | line_length: {line_length:.2f}"
-                                    # )
-                                    line_length = 0.0
 
-                            # print(line_length)
+                                self.car_detected = True
+
                             line_length += math.sqrt(
                                 a**2 + b**2 - 2 * a * b * math.cos(angle_increment)
                             )
+
                     except IndexError:
                         pass
+
+                if self.car_detected == True and self.car_detected_period > 0.5:
+                    self.car_detected = False
                 found_end = False
-                found_start = True
                 start_of_car = end_of_car
                 line_length = 0.0
 
