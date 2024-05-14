9d954cb818f73457a4e0bfdd3d951cf9b7108210
Luthov <lukettl15@gmail.com>
2024-05-14 14:59:36 +0800

    changed back scan and odom topics

--- f1tenth_ws/src/gap_finder/scripts/gap_finder_base.py
+++ f1tenth_ws/src/gap_finder/scripts/gap_finder_base.py
@@ -10,23 +10,24 @@ from nav_msgs.msg import Odometry
 
 from gap_finder.pid import PID
 
+
 # reference: https://github.com/f1tenth/f1tenth_labs_openrepo/blob/main/f1tenth_lab4/README.md
 class GapFinderAlgorithm:
-    def __init__(self, safety_bubble_diameter = 1, scan_angle_increment: float = 0.00435):
+    def __init__(self, safety_bubble_diameter=1, scan_angle_increment: float = 0.00435):
         self.safety_bubble_diameter = safety_bubble_diameter  # [m]
         self.scan_angle_increment = scan_angle_increment  # [rad]
         self.view_angle = 1.4  # [rad]
         self.range_max = 30
         self.max_range = 30
 
-        self.speed_pid = PID(Kp=-1., Ki=0.0, Kd=0.0)
+        self.speed_pid = PID(Kp=-1.0, Ki=0.0, Kd=0.0)
         self.speed_pid.set_point = 0.0
         self.steering_pid = PID(Kp=-1, Ki=0.0, Kd=0.005)
         self.steering_pid.set_point = 0.0
 
     def limit_field_of_view(self):
-        view_angle_count = self.view_angle//self.scan_angle_increment
-        lower_bound = int((len(self.ranges)- view_angle_count)/2)
+        view_angle_count = self.view_angle // self.scan_angle_increment
+        lower_bound = int((len(self.ranges) - view_angle_count) / 2)
         upper_bound = int(lower_bound + view_angle_count)
         self.ranges = self.ranges[lower_bound:upper_bound]
 
@@ -39,11 +40,12 @@ class GapFinderAlgorithm:
         # set the ranges of the safety bubble to 0
         self.front_range = self.ranges[int(len(self.ranges) // 2)]
         # make safety bubble increase with viewable distance
-        safety = self.safety_bubble_diameter + self.front_range/self.range_max*1.4
+        safety = self.safety_bubble_diameter + self.front_range / self.range_max * 1.4
         arc_increment = float(self.min_range * self.scan_angle_increment)
         radius_count = int(safety / 2 / arc_increment)
         for i in range(
-            max(0, self.min_range_index - radius_count), min(self.min_range_index + radius_count + 1, len(self.ranges))
+            max(0, self.min_range_index - radius_count),
+            min(self.min_range_index + radius_count + 1, len(self.ranges)),
         ):
             self.ranges[i] = 0.0
 
@@ -52,15 +54,21 @@ class GapFinderAlgorithm:
         # find the index of the max range in the ranges
         if self.min_range_index < len(self.ranges) // 2:
             # min is on right, turn left
-            self.max_gap_index = self.ranges.index(max(self.ranges[self.min_range_index :]))
+            self.max_gap_index = self.ranges.index(
+                max(self.ranges[self.min_range_index :])
+            )
         else:
             # min is on left, turn right
-            self.max_gap_index = self.ranges.index(max(self.ranges[: self.min_range_index]))
+            self.max_gap_index = self.ranges.index(
+                max(self.ranges[: self.min_range_index])
+            )
         self.max_range = self.ranges[self.max_gap_index]
 
     def find_twist(self):
         # find the twist required to go to the max range in the max gap
-        init_angZ = self.scan_angle_increment * (self.max_gap_index - len(self.ranges) // 2)
+        init_angZ = self.scan_angle_increment * (
+            self.max_gap_index - len(self.ranges) // 2
+        )
         angZ = self.steering_pid.update(init_angZ, self.dt)
         # linear velocity is proportional to the max range
         init_linX = self.front_range
@@ -82,14 +90,20 @@ class GapFinderNode(Node):
     def __init__(self, hz=50):
         super().__init__("gap_finder")
         # Scan Subscriber
-        self.scan_subscriber = self.create_subscription(LaserScan, "/gap_scan", self.scan_callback, 10)
+        self.scan_subscriber = self.create_subscription(
+            LaserScan, "/scan", self.scan_callback, 10
+        )
         self.scan_subscriber  # prevent unused variable warning
         # Odom Subscriber
-        self.odom_subscriber = self.create_subscription(Odometry, "/gap_odom", self.odom_callback, 10)
+        self.odom_subscriber = self.create_subscription(
+            Odometry, "/odom", self.odom_callback, 10
+        )
         self.odom_subscriber
         # Drive Publisher
-        self.drive_publisher = self.create_publisher(AckermannDriveStamped, "/nav/drive", 10)
-        self.timer = self.create_timer(1/hz , self.timer_callback)
+        self.drive_publisher = self.create_publisher(
+            AckermannDriveStamped, "/nav/drive", 10
+        )
+        self.timer = self.create_timer(1 / hz, self.timer_callback)
         # GapFinder Algorithm
         self.gapFinderAlgorithm = GapFinderAlgorithm()
         # Memory
@@ -101,7 +115,10 @@ class GapFinderNode(Node):
         self.last_time = self.current_time
 
     def get_time(self):
-        return self.get_clock().now().to_msg().sec + self.get_clock().now().to_msg().nanosec * 1e-9
+        return (
+            self.get_clock().now().to_msg().sec
+            + self.get_clock().now().to_msg().nanosec * 1e-9
+        )
 
     def scan_callback(self, scan_msg):
         self.ready = True
@@ -124,7 +141,7 @@ class GapFinderNode(Node):
         # steering limits
         if self.twist[1] > 0:
             self.twist[1] = min(self.twist[1], 0.35)
-        else: 
+        else:
             self.twist[1] = max(self.twist[1], -0.35)
         # speed limits
         self.twist[0] = max(self.twist[0], 0)
