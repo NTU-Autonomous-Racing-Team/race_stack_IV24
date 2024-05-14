f9fa02c6202d96ab22c03f901e9974c711729f8a
Luthov <lukettl15@gmail.com>
2024-05-14 15:40:23 +0800

    removing wall_follow files from PR

--- f1tenth_ws/src/wall_follow/scripts/wall_follow.py
+++ f1tenth_ws/src/wall_follow/scripts/wall_follow.py
@@ -7,49 +7,39 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from ackermann_msgs.msg import AckermannDriveStamped
 
-import numpy as np
-
 
 class WallFollow(Node):
 
     def __init__(self):
-        super().__init__("wall_follow")
-        self.declare_parameters(
-            namespace="", parameters=[("lookahead_distance_gain", None)]
-        )
+        super().__init__('wall_follow')
 
         # Subscribing to relevant topics
         self.sub_scan = self.create_subscription(
             LaserScan,
-            'wall_scan',
+            'scan',
             self.scan_callback,
             10)
         
         self.sub_odom = self.create_subscription(
             Odometry,
-            '/wall_odom',
+            '/odom',
             self.odom_callback,
             10)
 
-        self.sub_odom = self.create_subscription(
-            Odometry, "ego_racecar/odom", self.odom_callback, 10
-        )
-
-        self.pub_drive = self.create_publisher(AckermannDriveStamped, "drive", 10)
+        self.pub_drive = self.create_publisher(
+            AckermannDriveStamped,
+            'nav/drive',
+            10)
 
         self.sub_scan
         self.sub_odom
 
         self.drive_msg = AckermannDriveStamped()
-
-        self.lookahead_distance_gain = self.get_parameter(
-            "lookahead_distance_gain"
-        ).value
-
+        
         self.Kp = 0.25
         self.Ki = 0.006
         self.Kd = 0.001
-
+       
         self.integral = 0.0
         self.prev_error_1 = 0.0
         self.prev_secs = 0.0
@@ -58,90 +48,88 @@ class WallFollow(Node):
         self.longitudinal_vel = 0
         self.front_dist = 0
 
+        self.log = {"steering_angle": [], "speed": [], "actual_distance": []}
+
     def getRange(self, scan_data, angle):
         ranges = scan_data.ranges
-        angle_rad = angle * (np.pi / 180)
-        index = int(abs(angle_rad - scan_data.angle_max) / scan_data.angle_increment)
+        angle_rad = angle * (math.pi / 180)
+        index = int( abs(angle_rad - scan_data.angle_max) / scan_data.angle_increment )
 
         return ranges[index]
-
+    
     def odom_callback(self, odom_data):
         self.longitudinal_vel = odom_data.twist.twist.linear.x
 
+
     def scan_callback(self, scan_data):
+
+        ranges = scan_data.ranges
+        angle_min = scan_data.angle_min
+        angle_increment = scan_data.angle_increment
+        angle_max = scan_data.angle_max
 
         self.front_dist = self.getRange(scan_data, 0)
 
         secs = scan_data.header.stamp.sec
-        nsecs = scan_data.header.stamp.nanosec
+        nsecs = scan_data.header.stamp.nanosec 
 
         angle_b = 90
         angle_a = 40
-
-        theta = (angle_b - angle_a) * (np.pi / 180)
+        
+        theta = (angle_b - angle_a) * (math.pi / 180)
         # 90 Degrees to the car
-        distance_b = self.getRange(scan_data, angle_b)  # ranges[901]
+        distance_b = self.getRange(scan_data, angle_b) # ranges[901]
         # ~ 35 Degrees to the first scan
-        distance_a = self.getRange(scan_data, angle_a)  # ranges[760]
+        distance_a = self.getRange(scan_data, angle_a) # ranges[760]
+        
+        alpha = -1 * math.atan2( (distance_a * math.cos(theta) - distance_b) , (distance_a * math.sin(theta)) )
 
-        alpha = -1 * np.arctan2(
-            (distance_a * np.cos(theta) - distance_b), (distance_a * np.sin(theta))
-        )
-
-        actual_distance = distance_b * np.cos(alpha)
-        desired_distance = 1.2  # Metres
+        actual_distance = distance_b * math.cos(alpha)
+        desired_distance = 1.2 # Metres
 
         error = desired_distance - actual_distance
-        lookahead_distance = (
-            self.longitudinal_vel * self.lookahead_distance_gain
-        )  # Metres
+        lookahead_distance = self.longitudinal_vel * 0.45 # Metres
+        # lookahead_distance = self.longitudinal_vel * 0.15 # Metres
 
-        error_1 = error + lookahead_distance * np.sin(alpha)
-
-        if (
-            (self.prev_secs == 0.0)
-            & (self.prev_nsecs == 0.0)
-            & (self.prev_error_1 == 0.0)
-        ):
+        error_1 = error + lookahead_distance * math.sin(alpha)
+        
+        if (self.prev_secs == 0.0) & (self.prev_nsecs == 0.0) & (self.prev_error_1 == 0.0):
             self.prev_secs = secs
             self.prev_nsecs = nsecs
             self.prev_error_1 = error_1
 
         dt = secs - self.prev_secs + (nsecs - self.prev_nsecs) * 1e-9
-
-        try:
+        
+        
+        if dt != 0:
             self.integral += error_1 * dt
-
-            steering_angle = (
-                (self.Kp * error_1)
-                + (self.Ki * self.integral)
-                + (self.Kd * (error_1 - self.prev_error_1) / dt)
-            )
-
-            if steering_angle < -0.4:
-                self.drive_msg.drive.steering_angle = -0.4
-            elif steering_angle > 0.4:
-                self.drive_msg.drive.steering_angle = -0.4
-            else:
-                self.drive_msg.drive.steering_angle = steering_angle
-
-            steering_angle_degrees = abs(steering_angle * (180 / np.pi))
+            steering_angle = ( (self.Kp * error_1) + (self.Ki * self.integral) + (self.Kd * (error_1 - self.prev_error_1) / dt) )
+            self.drive_msg.drive.steering_angle = steering_angle
+            steering_angle_degrees = abs(steering_angle * (180 / math.pi))
 
             self.prev_error_1 = error_1
             self.prev_secs = secs
             self.prev_nsecs = nsecs
+            
 
-            self.drive_msg.drive.speed = 0.8 * (1 / 1.2) ** (
-                steering_angle_degrees - 15
-            )
+            # speed = 1 / exp(steering_angle_degrees)
+            #self.drive_msg.drive.speed = ((steering_angle_degrees / 5.2) - 3.8)**2
+
+            # steep drop
+            # self.drive_msg.drive.speed = (1 / 2)**(steering_angle_degrees - 3.9)
+
+            # less steep but still too much
+            # self.drive_msg.drive.speed = (1 / 1.5)**(steering_angle_degrees - 6.5)
+            # if self.front_dist < 2.5:
+            #     self.drive_msg.drive.speed = 2.0
+            # else:
+            self.drive_msg.drive.speed = -1 * float(min(0.5 * (1 / 1.2)**(steering_angle_degrees - 15), 10))
 
             self.pub_drive.publish(self.drive_msg)
-            self.get_logger().info(
-                f"{self.lookahead_distance_gain} | steering_angle: {steering_angle_degrees:.2f} | speed: {self.longitudinal_vel:.2f}"
-            )
-        except ZeroDivisionError:
-            pass
-
+            self.get_logger().info(f"steering_angle: {steering_angle_degrees:.2f} | speed: {self.longitudinal_vel:.2f} | {actual_distance:.2f} | {lookahead_distance:.2f}")
+            # self.log["steering_angle"].append(steering_angle_degrees)
+            # self.log["speed"].append(self.longitudinal_vel)
+            # self.log["actual_distance"].append(actual_distance)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -151,6 +139,7 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == "__main__":
 
+
+if __name__ == '__main__':
     main()
