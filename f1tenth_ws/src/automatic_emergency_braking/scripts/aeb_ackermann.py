c640059c0a2da21d4bfca32f540ca48e29c0d9d9
Luthov <lukettl15@gmail.com>
2024-06-04 02:03:50 +0800

    aeb less conservative

--- f1tenth_ws/src/automatic_emergency_braking/scripts/aeb_ackermann.py
+++ f1tenth_ws/src/automatic_emergency_braking/scripts/aeb_ackermann.py
@@ -14,8 +14,8 @@ from std_msgs.msg import Int32
 
 class AEB_Ackerman():
     def __init__(self, SAFETY_BUBBLE_DIAMETER = 0.3, 
-                 frictional_coefficient = 0.5, 
-                 max_steering_angle = 0.5,
+                 frictional_coefficient = 2.0, 
+                 max_steering_angle = 0.6,
                  wheelbase = 0.33,
                  view_angle = 3.142/2.0):
         # CONSTANTS
